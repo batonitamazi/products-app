5081ef61cfab4bf0928013fd0ddcc9ae29b134dc
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-05-06 21:37:42 +0400

    added Product List component modified App file

---
+++ products-app/src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+export default class ProductList extends Component {
+    render() {
+        return( 
+            <div className='ui unstackable items'>
+                Hello, Friend, I am a basic React Component
+            </div>
+        );
+    }
+}
