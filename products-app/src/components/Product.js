262081bf25fcc5cc0a01d1cd07bb04b31df13380
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-05-07 10:04:59 +0400

    added data to map on page

--- products-app/src/components/Product.js
+++ products-app/src/components/Product.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react'
 import '../App.css'
 
 export default class Product extends Component {
+    
     render() {
         return (
             <div className='item'>
                 <div className='image'>
-                    <img  className='main image'src='images/products/image-aqua.png'/>
+                    <img  className='main image'src={this.props.productImageUrl} alt='product-image'/>
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a  onClick={this.handleUpVote}>
+                            <i className='upicon' ></i>
+                            
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className='description'>
-                        <a href=''>Fort Knight</a>
+                        <a href={this.props.url}>{this.props.title}</a>
                         <p>Authentic renaissance actors, delivered in just two weeks</p>
                     </div>
                     <div className='extra'>
                         <span>Submitted by:</span>
-                        <img  className='ui avatar image' src='images/avatars/daniel.jpg' />
+                        <img  className='ui avatar image' src={this.props.submitterAvatarUrl} alt='avatar'/>
                     </div>
                 </div>
             </div>
--- products-app/src/components/ProductList.js
+++ products-app/src/components/ProductList.js
@@ -1,10 +1,28 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Product from './Product';
+import products from '../seed';
 
 export default class ProductList extends Component {
+    
     render() {
-        return( 
-            <Product />
+        const prod = products.sort((a, b) => (
+             b.votes - a.votes
+        ));
+        const productComponents = prod.map((product) =>
+            ( 
+                <Product
+                    key={'product - ' + product.id}
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitterAvatarUrl={product.submitterAvatarUrl}
+                    productImageUrl={product.productImageUrl}
+                />
+        ));
+        return(
+            productComponents
         );
     }
 }
